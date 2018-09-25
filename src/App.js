901636ff3eaf56cd64c75282b2530a2799c1d639
Sean Morrison <sean1972@gmail.com>
2018-09-24 21:49:45 -0700

    Create the loadScript() function which will load the script that that calls the Google Maps API

--- src/App.js
+++ src/App.js
@@ -20,4 +20,18 @@ class App extends Component {
   }
 }
 
+/*
+<script async defer src="https://maps.googleapis.com/maps/api/js?libraries=geometry,drawing&key=AIzaSyCcy8mnVTHRmKX8ubNE38RuSV5et15HNiQ&v=3&callback=initMap">
+</script>
+*/
+
+function loadScript(url) {
+  var index = window.document.getElementsByTagName('script')[0]
+  var script = window.document.createElement('script')
+  script.src = url
+  script.async = true
+  script.defer = true
+  index.parentNode.insertBefore(script, index)
+}
+
 export default App;
