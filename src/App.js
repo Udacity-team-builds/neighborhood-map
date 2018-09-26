d6455cad77ac037382ed46e5a3963a51554cc841
Sean Morrison <sean1972@gmail.com>
2018-09-25 20:52:25 -0700

    Updated the latLong and getVenues() to locate near San Jose, CA

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import axios from 'axios';
 
 class App extends Component {
 
+  state = {
+    venues: []
+  }
+
   componentDidMount() {
     this.getVenues()
     this.renderMap()
@@ -21,13 +25,15 @@ class App extends Component {
       client_id: "HK5FLRYBMQ2AO2JYR4VVGU5ZXKB4B0N2B0YLOOGHCKXDD0EZ",
       client_secret: "SOZXIHAVVLHETXOGTSXOIOA5FKCPWUGNQCW1GC3L5TIF31PK",
       query: "food",
-      near: "Sydney",
+      near: "San Jose",
       v: "20182507"
     }
 
     axios.get(endPoint + new URLSearchParams(parameters))
       .then(response => {
-        console.log(response.data.response.groups[0].items)
+        this.setState({
+          venues: response.data.response.groups[0].items
+        })
       })
       .catch(error => {
         console.log("ERROR!! " + error)
@@ -36,7 +42,7 @@ class App extends Component {
 
   initMap = () => {
     var map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
+      center: {lat: 37.338208, lng: -121.886329},
       zoom: 8
     });
   }
