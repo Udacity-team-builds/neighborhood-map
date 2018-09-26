afae10240331478e26411b6cfe7246143c67dc54
Sean Morrison <sean1972@gmail.com>
2018-09-25 20:40:24 -0700

    Create getVenues() function in App.js to fetch location data using Foursquare API and Axios

--- src/App.js
+++ src/App.js
@@ -6,12 +6,32 @@ import axios from 'axios';
 class App extends Component {
 
   componentDidMount() {
+    this.getVenues()
     this.renderMap()
   }
 
   renderMap = () => {
     loadScript("https://maps.googleapis.com/maps/api/js?libraries=geometry,drawing&key=AIzaSyCcy8mnVTHRmKX8ubNE38RuSV5et15HNiQ&v=3&callback=initMap")
     window.initMap = this.initMap
+  }
+
+  getVenues = () => {
+    const endPoint = "https://api.foursquare.com/v2/venues/explore?"
+    const parameters = {
+      client_id: "HK5FLRYBMQ2AO2JYR4VVGU5ZXKB4B0N2B0YLOOGHCKXDD0EZ",
+      client_secret: "SOZXIHAVVLHETXOGTSXOIOA5FKCPWUGNQCW1GC3L5TIF31PK",
+      query: "food",
+      near: "Sydney",
+      v: "20182507"
+    }
+
+    axios.get(endPoint + new URLSearchParams(parameters))
+      .then(response => {
+        console.log(response.data.response.groups[0].items)
+      })
+      .catch(error => {
+        console.log("ERROR!! " + error)
+      })
   }
 
   initMap = () => {
