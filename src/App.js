3dc55e969c43d446f8c1985eec605214d93b1c5f
Sean Morrison <sean1972@gmail.com>
2018-10-04 20:39:28 -0700

    Added Google and Foursquare keys back into the project

--- src/App.js
+++ src/App.js
@@ -1,96 +1,103 @@
 import React, { Component } from 'react';
 import './App.css';
-
 import axios from 'axios';
 
 class App extends Component {
-
-  state = {
-    venues: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      venues: [],
+      allMarkers: []
+    };
   }
 
   componentDidMount() {
-    this.getVenues()
+    this.getVenues();
   }
 
   renderMap = () => {
-    loadScript("https://maps.googleapis.com/maps/api/js?libraries=geometry,drawing&key=AIzaSyCcy8mnVTHRmKX8ubNE38RuSV5et15HNiQ&v=3&callback=initMap")
-    window.initMap = this.initMap
-  }
+    loadScript(
+      'https://maps.googleapis.com/maps/api/js?libraries=geometry,drawing&key=AIzaSyCcy8mnVTHRmKX8ubNE38RuSV5et15HNiQ&v=3&callback=initMap'
+    );
+    window.initMap = this.initMap;
+  };
 
   getVenues = () => {
-    const endPoint = "https://api.foursquare.com/v2/venues/explore?"
+    const endPoint = 'https://api.foursquare.com/v2/venues/explore?';
     const parameters = {
-      client_id: "HK5FLRYBMQ2AO2JYR4VVGU5ZXKB4B0N2B0YLOOGHCKXDD0EZ",
-      client_secret: "SOZXIHAVVLHETXOGTSXOIOA5FKCPWUGNQCW1GC3L5TIF31PK",
-      query: "food",
-      near: "San Jose",
-      v: "20182507"
-    }
+      client_id: 'HK5FLRYBMQ2AO2JYR4VVGU5ZXKB4B0N2B0YLOOGHCKXDD0EZ',
+      client_secret: 'SOZXIHAVVLHETXOGTSXOIOA5FKCPWUGNQCW1GC3L5TIF31PK',
+      query: 'food',
+      near: 'San Jose',
+      v: '20182507'
+    };
 
-    axios.get(endPoint + new URLSearchParams(parameters))
+    axios
+      .get(endPoint + new URLSearchParams(parameters))
       .then(response => {
-        this.setState({
-          venues: response.data.response.groups[0].items
-        }, this.renderMap())
+        this.setState(
+          {
+            venues: response.data.response.groups[0].items
+          },
+          this.renderMap()
+        );
       })
       .catch(error => {
-        console.log("ERROR!! " + error)
-      })
-  }
+        console.log('ERROR!! ' + error);
+      });
+  };
 
   initMap = () => {
-
     // Create A Map
     var map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: 37.338208, lng: -121.886329},
+      center: { lat: 37.338208, lng: -121.886329 },
       zoom: 13
-    })
+    });
 
     // Create An Info Window
-    var infowindow = new window.google.maps.InfoWindow()
+    var infowindow = new window.google.maps.InfoWindow();
 
     // Display Dynamic Markers
     this.state.venues.map(myVenue => {
-
-      var contentString = `${myVenue.venue.name}`
+      var contentString = `${myVenue.venue.name}`;
 
       // Create A Marker
       var marker = new window.google.maps.Marker({
-        position: {lat: myVenue.venue.location.lat, lng: myVenue.venue.location.lng},
+        position: {
+          lat: myVenue.venue.location.lat,
+          lng: myVenue.venue.location.lng
+        },
         map: map,
         title: myVenue.venue.name
-      })
+      });
 
       // Click On A Marker
       marker.addListener('click', function() {
-
         // Change The Content
-        infowindow.setContent(contentString)
+        infowindow.setContent(contentString);
 
         // Open An InfoWindow
         infowindow.open(map, marker);
-      })
-
-    })
-  }
+      });
+    });
+  };
 
   render() {
     return (
       <main>
-        <div id="map"></div>
+        <div id="map" />
       </main>
     );
   }
 }
 
 function loadScript(url) {
-  var index = window.document.getElementsByTagName('script')[0]
-  var script = window.document.createElement('script')
-  script.src = url
-  script.async = true
-  script.defer = true
-  index.parentNode.insertBefore(script, index)
+  var index = window.document.getElementsByTagName('script')[0];
+  var script = window.document.createElement('script');
+  script.src = url;
+  script.async = true;
+  script.defer = true;
+  index.parentNode.insertBefore(script, index);
 }
 
 export default App;
