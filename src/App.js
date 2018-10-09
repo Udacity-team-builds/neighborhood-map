0659bae099b641486a1ba8c9d46ed5c4096571ec
Sean Morrison <sean1972@gmail.com>
2018-10-08 21:31:07 -0700

    Messing around with things

--- src/App.js
+++ src/App.js
@@ -6,20 +6,17 @@ import Map from './components/Map';
 import axios from 'axios';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      venues: [],
-      allMarkers: []
+  state = {
+      venues: []
     };
   }
 
   componentDidMount() {
-    this.localPlaces();
-    document.title = 'Places around the South Bay Area';
+    this.getVenues();
+    document.title = 'My Favorite Bay Area Ice Cream Places';
   }
 
-  startMap = () => {
+  renderMap = () => {
     const GoogleMapsAPI = 'AIzaSyCcy8mnVTHRmKX8ubNE38RuSV5et15HNiQ';
     loadScript(
       `https://maps.googleapis.com/maps/api/js?libraries=geometry,drawing&key=${GoogleMapsAPI}&v=3&callback=initMap`
@@ -27,64 +24,65 @@ class App extends Component {
     window.initMap = this.initMap;
   };
 
-  localPlaces = () => {
-    const endPoint = 'https://api.foursquare.com/v2/venues/explore?';
-    const parameters = {
-      client_id: 'HK5FLRYBMQ2AO2JYR4VVGU5ZXKB4B0N2B0YLOOGHCKXDD0EZ',
-      client_secret: 'SOZXIHAVVLHETXOGTSXOIOA5FKCPWUGNQCW1GC3L5TIF31PK',
-      query: 'icecream',
-      near: 'San Jose,CA',
-      v: '20182507'
-    };
-
-    axios
-      .get(endPoint + new URLSearchParams(parameters))
-      .then(response => {
-        this.setState(
-          {
-            venues: response.data.response.groups[0].items
-          },
-          this.startMap()
-        );
-      })
-      .catch(error => {
-        console.log(
-          'ERROR: ' + error + '. Please refresh your page to try again.'
-        );
-      });
+getVenues = () => {
+  const endPoint = 'https://api.foursquare.com/v2/venues/explore?';
+  const parameters = {
+    client_id: 'HK5FLRYBMQ2AO2JYR4VVGU5ZXKB4B0N2B0YLOOGHCKXDD0EZ',
+    client_secret: 'SOZXIHAVVLHETXOGTSXOIOA5FKCPWUGNQCW1GC3L5TIF31PK',
+    query: 'icecream',
+    ll: '37.338208, -121.886329',
+    radius: '1000',
+    v: '20180910',
+    limit: 12
   };
 
-  initMap = () => {
-    // Create An Info Window
-    let infowindow = new window.google.maps.InfoWindow();
+  axios
+    .get(endPoint + new URLSearchParams(parameters))
+    .then(response => {
+      this.setState(
+        {
+          venues: response.data.response.groups[0].items
+        },
+        this.renderMap()
+      );
+    })
+    .catch(error => {
+      console.log('ERROR! ' + error);
+    });
+};
 
-    // Create A Map
+  initMap = () => {
     const map = new window.google.maps.Map(document.getElementById('map'), {
       center: { lat: 37.338208, lng: -121.886329 },
-      zoom: 12
+      zoom: 14
     });
-
-    // Display Dynamic Markers
+    const infowindow = new window.google.maps.InfoWindow();
     this.state.venues.forEach(myVenue => {
-      let nameOfVenue = `${myVenue.venue.name}`;
-
-      // Create A Marker
-      let marker = new window.google.maps.Marker({
+      const infoString = `<h4>${myVenue.venue.name}</h4><p>Street Address: ${
+        myVenue.venue.location.address
+      }</p><p>Distance from Harpa: ${
+        myVenue.venue.location.distance
+      } meters</p>`;
+      const marker = new window.google.maps.Marker({
         position: {
           lat: myVenue.venue.location.lat,
           lng: myVenue.venue.location.lng
         },
         map: map,
-        animation: window.google.maps.Animation.DROP,
-        title: myVenue.venue.name
+        title: myVenue.venue.name,
+        id: myVenue.venue.id,
+        animation: window.google.maps.Animation.DROP
       });
-
-      // Click On A Marker
       marker.addListener('click', function() {
-        // Change The Content
-        infowindow.setContent(nameOfVenue);
-
-        // Open An InfoWindow
+        if (marker.getAnimation() !== null) {
+          marker.setAnimation(null);
+        } else {
+          marker.setAnimation(window.google.maps.Animation.BOUNCE);
+        }
+        setTimeout(() => {
+          marker.setAnimation(null);
+        }, 1000);
+        infowindow.setContent(infoString);
         infowindow.open(map, marker);
       });
     });
@@ -92,10 +90,10 @@ class App extends Component {
 
   render() {
     return (
-      <div className="app">
+      <div id="app-container"className="app">
         <div className="d-flex flex-row bd-highlight mb-3">
           <Header />
-          <SearchBar />
+          <SearchBar venues={this.state.venues} />
           <Map />
         </div>
       </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
+import escapeRegExp from 'escape-string-regexp';
 
 class SearchBar extends Component {
+  state = {
+    query: ''
+  };
+
+  updateQuery = query => {
+    this.setState({ query: query.trim() });
+  };
   render() {
+    let showingVenues;
+    if (this.state.query) {
+      const match = new RegExp(escapeRegExp(this.state.query), 'i');
+      showingVenues = this.props.venues.filter(venue => match.test(venue.name));
+    } else {
+      showingVenues = this.props.venues;
+    }
+
     return (
       <div id="search-bar" className="p-2 bd-highlight">
+        <div className="list-group">
+          <input
+            className="form-control"
+            type="text"
+            value={this.state.query}
+            onChange={e => this.updateQuery(e.target.value)}
+            aria-label="Text input"
+            placeholder="Filtered search"
+          />
+        </div>
         <ul>
-          <li>Item X</li>
-          <li>Item X</li>
-          <li>Item X</li>
-          <li>Item X</li>
-          <li>Item X</li>
-          <li>Item X</li>
+          {showingVenues.map(showingVenue => (
+            <li key={showingVenue.venue.id}>
+              <p>{showingVenue.venue.name}</p>
+            </li>
+          ))}
         </ul>
       </div>
     );
