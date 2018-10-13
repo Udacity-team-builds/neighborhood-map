f137cb3674ebd70696ed54415bca9ace4e7b8615
Sean Morrison <sean1972@gmail.com>
2018-10-13 14:28:40 -0700

    Still trying to fix branch merge

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import escapeRegExp from 'escape-string-regexp';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 import Map from './components/Map';
-import ErrorCatcher from './components/ErrorCatcher';
-import escapeRegExp from 'escape-string-regexp';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.css';
 import CatchErrors from './components/CatchErrors';
@@ -15,18 +13,10 @@ class App extends Component {
     super(props);
     this.state = {
       venues: [],
-<<<<<<< HEAD
-      allVenues: [],
-      allMarkers: [],
-      query: ''
-||||||| merged common ancestors
-      allMarkers: []
-=======
       allVenues: [],
       markers: [],
       hiddenMarkers: [],
       query: ''
->>>>>>> MapApp
     };
   }
 
@@ -195,26 +185,10 @@ class App extends Component {
 
     // Display Dynamic Markers
     this.state.venues.forEach(myVenue => {
-<<<<<<< HEAD
-      const markerImage = 'http://techsnazzy.com/assets/img/ice-cream.png';
-      // Create A Marker
-||||||| merged common ancestors
-      const infoString = `
-        <h4>${myVenue.venue.name}</h4>
-        <p>${myVenue.venue.location.address}<br>${
-        myVenue.venue.location.city
-      }, ${myVenue.venue.location.state} ${
-        myVenue.venue.location.postalCode
-      }</p>
-        `;
-      const markerImage = 'http://techsnazzy.com/assets/img/ice-cream.png';
-      // Create A Marker
-=======
       // Create a Marker Icon
       const markerImage = 'https://techsnazzy.com/assets/img/ice-cream.png';
 
       // Create a Marker
->>>>>>> MapApp
       const marker = new window.google.maps.Marker({
         position: {
           lat: myVenue.venue.location.lat,
@@ -227,42 +201,10 @@ class App extends Component {
         animation: window.google.maps.Animation.DROP
       });
 
-<<<<<<< HEAD
-      const infoString = `
-        <h4>${myVenue.venue.name}</h4>
-        <p>${myVenue.venue.location.address}<br>${
-        myVenue.venue.location.city
-      }, ${myVenue.venue.location.state} ${
-        myVenue.venue.location.postalCode
-      }</p>
-        `;
-
-      // Click On A Marker
-||||||| merged common ancestors
-      // Click On A Marker
-=======
       this.state.markers.push(marker);
 
       // Click on a Marker
->>>>>>> MapApp
       marker.addListener('click', function() {
-<<<<<<< HEAD
-        // Change The Content
-        infowindow.setContent(infoString);
-        if (marker.getAnimation() !== null) {
-          marker.setAnimation(null);
-        } else {
-          marker.setAnimation(window.google.maps.Animation.BOUNCE);
-        }
-        setTimeout(() => {
-||||||| merged common ancestors
-        if (marker.getAnimation() !== null) {
-          marker.setAnimation(null);
-        } else {
-          marker.setAnimation(window.google.maps.Animation.BOUNCE);
-        }
-        setTimeout(() => {
-=======
         infowindow.setContent(`
         <h4>${myVenue.venue.name}</h4>
         <p>${myVenue.venue.location.address}
@@ -280,20 +222,10 @@ class App extends Component {
 
         marker.setAnimation(window.google.maps.Animation.BOUNCE);
         window.setTimeout(function() {
->>>>>>> MapApp
           marker.setAnimation(null);
         }, 1000);
-<<<<<<< HEAD
-
-        // Open An InfoWindow
-||||||| merged common ancestors
-        // Change The Content
-        infowindow.setContent(infoString);
-        // Open An InfoWindow
-=======
 
         // Open an InfoWindow
->>>>>>> MapApp
         infowindow.open(map, marker);
       });
 
@@ -304,32 +236,6 @@ class App extends Component {
     });
   };
 
-<<<<<<< HEAD
-  //Handling the search box changes
-  handleSearch = query => {
-    this.setState({ query });
-    var filterVenues;
-    var hiddenMarkers;
-    this.state.markers.map(marker => marker.setVisible(true));
-    if (query) {
-      const match = new RegExp(escapeRegExp(query), 'i');
-      filterVenues = this.state.venues.filter(site =>
-        match.test(site.venue.name)
-      );
-      this.setState({ venues: filterVenues });
-      hiddenMarkers = this.state.markers.filter(marker =>
-        filterVenues.every(site => site.venue.name !== marker.title)
-      );
-      this.itemVisibility(hiddenMarkers, false);
-      this.setState({ hiddenMarkers });
-    } else {
-      this.setState({ venues: this.state.allVenues });
-      this.itemVisibility(this.state.markers, true);
-    }
-  };
-
-||||||| merged common ancestors
-=======
   markerVisibility = (arr, boo) => {
     return arr.forEach(marker => marker.setVisible(boo));
   };
@@ -356,34 +262,7 @@ class App extends Component {
     }
   };
 
->>>>>>> MapApp
   render() {
-<<<<<<< HEAD
-    return (
-      <ErrorCatcher>
-        <div className="App" role="main">
-          <Header />
-          <div className="d-flex flex-row bd-highlight mb-3">
-            <SearchBar
-              className="p-2 bd-highlight"
-              markers={this.state.markers}
-              filteredVenues={this.filteredVenues}
-              query={this.state.query}
-              clearQuery={this.clearQuery}
-              handleSearch={b => this.handleSearch(b)}
-              clickLocation={this.clickLocation}
-              venues={this.state.venues}
-            />
-            <Map className="p-2 bd-highlight" />
-          </div>
-||||||| merged common ancestors
-    return (
-      <div className="App" role="main">
-        <Header />
-        <div className="d-flex flex-row bd-highlight mb-3">
-          <SearchBar className="p-2 bd-highlight" venues={this.state.venues} />
-          <Map className="p-2 bd-highlight" />
-=======
     if (this.state.hasError) {
       return <div aria-label="Error">Oops, Something went wrong!</div>;
     } else {
@@ -405,49 +284,10 @@ class App extends Component {
               <Map className="p-2 bd-highlight" />
             </div>
           </CatchErrors>
->>>>>>> MapApp
         </div>
-<<<<<<< HEAD
-      </ErrorCatcher>
-    );
-||||||| merged common ancestors
-      </div>
-    );
-=======
       );
     }
->>>>>>> MapApp
   }
 }
 
-<<<<<<< HEAD
-function loadScript(url) {
-  var index = window.document.getElementsByTagName('script')[0];
-  var script = window.document.createElement('script');
-  script.src = url;
-  script.async = true;
-  script.defer = true;
-  index.parentNode.insertBefore(script, index);
-  script.onerror = function() {
-    document.write('Error! Map was not loaded.');
-  };
-}
-
-||||||| merged common ancestors
-function loadScript(url) {
-  var index = window.document.getElementsByTagName('script')[0];
-  var script = window.document.createElement('script');
-  script.src = url;
-  script.async = true;
-  script.defer = true;
-  index.parentNode.insertBefore(script, index);
-  script.onerror = function() {
-    document.write(
-      'There was an error loading the Google Map. Please refresh your page to try again.'
-    );
-  };
-}
-
-=======
->>>>>>> MapApp
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,54 +9,9 @@ class SearchBar extends Component {
     });
   };
 
-<<<<<<< HEAD
-  updateQuery = query => {
-    this.setState({ query: query.trim() });
-  };
-
-  openInfo = locationId => {
-    this.props.markers.forEach(marker => {
-      if (marker.id === locationId) {
-        window.google.maps.event.trigger(marker, 'click');
-      }
-    });
-  };
-||||||| merged common ancestors
-  updateQuery = query => {
-    this.setState({ query: query.trim() });
-  };
-=======
->>>>>>> MapApp
   render() {
     return (
       <div id="search-bar" className="p-2 bd-highlight">
-<<<<<<< HEAD
-        <div>
-          <input
-            name="searchVenuesInput"
-            className="form-control text-center"
-            type="text"
-            aria-label="Locate ice cream"
-            value={this.state.query}
-            onChange={event => this.updateQuery(event.target.value)}
-            placeholder="Search Venues"
-            tabIndex={1}
-          />
-        </div>
-||||||| merged common ancestors
-        <div>
-          <label for="searchVenuesInput" />
-          <input
-            name="searchVenuesInput"
-            className="form-control text-center"
-            type="text"
-            value={this.state.query}
-            onChange={event => this.updateQuery(event.target.value)}
-            placeholder="Search Venues"
-            tabIndex={1}
-          />
-        </div>
-=======
         <input
           className="form-control text-center"
           type="text"
@@ -66,7 +21,6 @@ class SearchBar extends Component {
           value={this.props.query}
           onChange={event => this.props.handleSearch(event.target.value)}
         />
->>>>>>> MapApp
         <ul className="list-group">
           {this.props.venues.map(listVenues => (
             <li
